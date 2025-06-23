d08ccdd4925232db0c0c05858b5588f1faf9e660
italogfarias <italogabriel543i@gmail.com>
2025-06-23 16:13:28 -0300

    Initial commit

---
+++ LocalizadorDeCep/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace LocalizadorDeCep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbCep = new System.Windows.Forms.MaskedTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.grbResultado = new System.Windows.Forms.GroupBox();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.lblLogradouro = new System.Windows.Forms.Label();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.lblUnidade = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblLocalidade = new System.Windows.Forms.Label();
+            this.lblUf = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblRegiao = new System.Windows.Forms.Label();
+            this.lblDdd = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grbResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txbCep
+            // 
+            this.txbCep.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCep.Location = new System.Drawing.Point(120, 135);
+            this.txbCep.Mask = "00000-000";
+            this.txbCep.Name = "txbCep";
+            this.txbCep.Size = new System.Drawing.Size(141, 36);
+            this.txbCep.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LocalizadorDeCep.Properties.Resources.street_map;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Localizador de CEP";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBuscar.Location = new System.Drawing.Point(102, 225);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(172, 41);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar CEP";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // grbResultado
+            // 
+            this.grbResultado.Controls.Add(this.lblDdd);
+            this.grbResultado.Controls.Add(this.lblRegiao);
+            this.grbResultado.Controls.Add(this.lblEstado);
+            this.grbResultado.Controls.Add(this.lblUf);
+            this.grbResultado.Controls.Add(this.lblLocalidade);
+            this.grbResultado.Controls.Add(this.lblBairro);
+            this.grbResultado.Controls.Add(this.lblUnidade);
+            this.grbResultado.Controls.Add(this.lblComplemento);
+            this.grbResultado.Controls.Add(this.lblLogradouro);
+            this.grbResultado.Controls.Add(this.lblCep);
+            this.grbResultado.Location = new System.Drawing.Point(400, 41);
+            this.grbResultado.Name = "grbResultado";
+            this.grbResultado.Size = new System.Drawing.Size(398, 346);
+            this.grbResultado.TabIndex = 4;
+            this.grbResultado.TabStop = false;
+            this.grbResultado.Text = "V";
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Location = new System.Drawing.Point(22, 30);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(31, 13);
+            this.lblCep.TabIndex = 0;
+            this.lblCep.Text = "CEP:";
+            // 
+            // lblLogradouro
+            // 
+            this.lblLogradouro.AutoSize = true;
+            this.lblLogradouro.Location = new System.Drawing.Point(22, 55);
+            this.lblLogradouro.Name = "lblLogradouro";
+            this.lblLogradouro.Size = new System.Drawing.Size(64, 13);
+            this.lblLogradouro.TabIndex = 1;
+            this.lblLogradouro.Text = "Logradouro:";
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.Location = new System.Drawing.Point(22, 84);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(74, 13);
+            this.lblComplemento.TabIndex = 2;
+            this.lblComplemento.Text = "Complemento:";
+            // 
+            // lblUnidade
+            // 
+            this.lblUnidade.AutoSize = true;
+            this.lblUnidade.Location = new System.Drawing.Point(22, 110);
+            this.lblUnidade.Name = "lblUnidade";
+            this.lblUnidade.Size = new System.Drawing.Size(50, 13);
+            this.lblUnidade.TabIndex = 3;
+            this.lblUnidade.Text = "Unidade:";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(22, 139);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(37, 13);
+            this.lblBairro.TabIndex = 4;
+            this.lblBairro.Text = "Bairro:";
+            // 
+            // lblLocalidade
+            // 
+            this.lblLocalidade.AutoSize = true;
+            this.lblLocalidade.Location = new System.Drawing.Point(22, 166);
+            this.lblLocalidade.Name = "lblLocalidade";
+            this.lblLocalidade.Size = new System.Drawing.Size(62, 13);
+            this.lblLocalidade.TabIndex = 5;
+            this.lblLocalidade.Text = "Localidade:";
+            // 
+            // lblUf
+            // 
+            this.lblUf.AutoSize = true;
+            this.lblUf.Location = new System.Drawing.Point(22, 198);
+            this.lblUf.Name = "lblUf";
+            this.lblUf.Size = new System.Drawing.Size(24, 13);
+            this.lblUf.TabIndex = 6;
+            this.lblUf.Text = "UF:";
+            this.lblUf.Click += new System.EventHandler(this.lblUf_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(22, 226);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(43, 13);
+            this.lblEstado.TabIndex = 7;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // lblRegiao
+            // 
+            this.lblRegiao.AutoSize = true;
+            this.lblRegiao.Location = new System.Drawing.Point(22, 261);
+            this.lblRegiao.Name = "lblRegiao";
+            this.lblRegiao.Size = new System.Drawing.Size(44, 13);
+            this.lblRegiao.TabIndex = 8;
+            this.lblRegiao.Text = "Região:";
+            // 
+            // lblDdd
+            // 
+            this.lblDdd.AutoSize = true;
+            this.lblDdd.Location = new System.Drawing.Point(22, 295);
+            this.lblDdd.Name = "lblDdd";
+            this.lblDdd.Size = new System.Drawing.Size(34, 13);
+            this.lblDdd.TabIndex = 9;
+            this.lblDdd.Text = "DDD:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 399);
+            this.Controls.Add(this.grbResultado);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txbCep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grbResultado.ResumeLayout(false);
+            this.grbResultado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txbCep;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox grbResultado;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label lblUnidade;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Label lblLogradouro;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.Label lblUf;
+        private System.Windows.Forms.Label lblLocalidade;
+        private System.Windows.Forms.Label lblDdd;
+        private System.Windows.Forms.Label lblRegiao;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
+
---
+++ LocalizadorDeCep/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace LocalizadorDeCep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblUf_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string cep = txbCep.Text;
+            string url = $"https://viacep.com.br/ws/{cep}/json/";
+            if(cep.Length < 8)
+            {
+                MessageBox.Show("CEP inválido", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                //Limita a utilização do webClient dentro do escopo:
+                using (WebClient navegador = new WebClient())
+                {
+                    string conteudo = navegador.DownloadString(url);
+                    // Converter para UTF-8 (corrigir acentuação)
+                    conteudo = Encoding.UTF8.GetString(Encoding.Default.GetBytes(conteudo));
+                    //Instanciar um obj do tipo endereco
+                    Endereco endereco = new Endereco();
+
+                    endereco = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+                    // Verificar se endereco.Cep não é nulo:
+                    if (!string.IsNullOrEmpty(endereco.Cep))
+                    {
+
+                        lblCep.Text = $"CEP:{endereco.Cep}";
+                        lblBairro.Text = $"Bairro:{endereco.Bairro}";
+                        lblComplemento.Text = $"Complemento:{endereco.Complemento}";
+                        lblDdd.Text = $"DDD:{endereco.Ddd}";
+                        lblRegiao.Text = $"Região:{endereco.Regiao}";
+                        lblUnidade.Text = $"Unidade:{endereco.Unidade}";
+                        lblLogradouro.Text = $"Logradouro:{endereco.Logradouro}";
+                        lblUf.Text = $"UF:{endereco.Uf}";
+                        lblEstado.Text = $"Estado:{endereco.Estado}";
+                        lblLocalidade.Text = $"Localidade:{endereco.Localidade}";
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("CEP não encontrado! ");
+                    }
+                }
+               
+            }
+        }
+    }
+}
